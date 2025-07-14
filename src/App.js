bd10bf20eac370a20c84ce67a4d1dfdae929d554
Dhyan U P <dhyanup2003@gmail.com>
2025-07-15 01:04:21 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-
+import Addpost from './components/Addpost';
+import Searchpost from './components/Searchpost';
+import Deletepost from './components/Deletepost';
+import Viewall from './components/Viewall';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
- <BrowserRouter>
- <Routes>
-  <Route></Route>
- </Routes>
- </BrowserRouter>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Addpost />} />
+    <Route path="/delete" element={<Deletepost />} />
+    <Route path="/search" element={<Searchpost />} />
+    <Route path="/viewall" element={<Viewall />} />
+
+    
+
+
+   </Routes>
+   </BrowserRouter>
   );
 }
 
